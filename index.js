d8d57f19d2fc8b25d946e3c67d3499bf6e164119
Rohan19258 <109348242+Rohan19258@users.noreply.github.com>
2022-11-30 09:53:58 +0530

    Add files via upload
    done

---
+++ index.js
@@ -0,0 +1,37 @@
+const express=require('express')
+const mongoose=require('mongoose')
+const port=process.env.PORT || 8000
+const ShortUrl=require('./models/shorturl')
+const app=express()
+
+app.set('view engine','ejs')
+app.set('views','view')
+app.use(express.urlencoded({ extended: false }))
+
+
+const db=""
+mongoose.connect(db,{Usenewurlparser: true, useUnifiedTopology:true})
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', { shortUrls: shortUrls })
+  })
+  
+  app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
+  
+    res.redirect('/')
+  })
+  
+  app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+    if (shortUrl == null) return res.sendStatus(404)
+  
+    shortUrl.clicks++
+    shortUrl.save()
+  
+    res.redirect(shortUrl.full)
+  })
+
+
+app.listen(port,console.log(`server is listening on ${port}`))
